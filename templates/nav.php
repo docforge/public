5d049d46a05195fb47a692df12bedb03b881af85
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-07-02 19:54:39 +0200

    First commit

--- templates/nav.php
+++ templates/nav.php
@@ -14,35 +14,6 @@
 $scope = $this->getScope();
 ?>
 
-<nav class="tm-navbar uk-navbar uk-navbar-attached">
-    <div class="uk-container uk-container-center">
-
-        <a href="/" class="uk-navbar-brand dw-navbar-brand uk-hidden-small">
-            <i class="uk-icon-logo"></i>
-
-        </a>
-
-        <ul id="dw-navbar-menu" class="uk-navbar-nav uk-hidden-small">
-
-            <li>
-                <div class="nav-search">
-                    <div class="gcse-searchbox-only"></div>
-                </div>
-            </li>
-        </ul>
-
-        <a href="#tm-offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas=""></a>
-
-        <div class="uk-navbar-brand uk-navbar-center uk-visible-small">
-            <a href="/" class="dw-navbar-brand"><i class="uk-icon-logo"></i> <?=$scope->getName()?>
-            </a>
-        </div>
-
-    </div>
-
-</nav>
-
-
 <header class="navbar">
     <div class="sidebar-button">
         <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" viewBox="0 0 448 512" class="icon">
@@ -59,34 +30,11 @@ $scope = $this->getScope();
         <nav class="nav-links can-hide">
             <?php foreach ($scope->listRootPages() as $page) { ?>
                 <div class="nav-item">
-                    <a class="nav-link <?=$page->isCurrent()||$page->isParentOfCurrent()?'router-link-active':''?>"
-                            href="<?=$page->getUrl()?>">
+                    <a href="<?=$page->getUrl()?>" class="nav-link <?=$page->isCurrent()||$page->isParentOfCurrent()?'router-link-active':''?>">
                         <?=$page->getMenuLabel()?>
                     </a>
                 </div>
             <?php } ?>
-
-
-
-
-            <div class="nav-item"><a href="/theme/" class="nav-link ">
-                    Theme
-                </a></div>
-            <div class="nav-item"><a href="/integrations/" class="nav-link">
-                    Integrations
-                </a></div>
-            <div class="nav-item"><a href="/about/" class="nav-link">
-                    About
-                </a></div>
-            <a href="https://github.com/stefanobartoletti/bricks" target="_blank" rel="noopener noreferrer"
-               class="repo-link">
-                GitHub
-                <span><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px"
-                           viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor"
-                                                                                                    d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon
-                                fill="currentColor"
-                                points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg> <span
-                            class="sr-only">(opens new window)</span></span></a>
         </nav>
     </div>
 </header>
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -15,23 +15,47 @@ $scope = $this->getScope();
 $currentRootPage = $scope->getCurrentRootPage();
 ?>
 
-<ul class="tm-nav uk-nav">
+<div class="sidebar-mask"></div>
 
-    <?php if (!$currentRootPage->hasSubPages()) { ?>
-        <?php if ($scope->hasNonTerminalRootPages()) { ?>
-            <?php foreach ($scope->listNonTerminalRootPages() as $page) { ?>
-                <li class="uk-nav-header"><?=$page->getLabel()?></li>
-                <?php foreach ($page->listSubPages() as $subPage) { ?>
+<aside class="sidebar">
+    <nav class="nav-links">
+        <?php foreach ($scope->listRootPages() as $page) { ?>
+            <div class="nav-item">
+                <a href="<?=$page->getUrl()?>" class="nav-link <?=$page->isCurrent()||$page->isParentOfCurrent()?'router-link-active':''?>">
+                    <?=$page->getMenuLabel()?>
+                </a>
+            </div>
+        <?php } ?>
+    </nav>
+
+    <ul class="sidebar-links">
+        <?php if (!$currentRootPage->hasSubPages()) { ?>
+            <?php if ($scope->hasNonTerminalRootPages()) { ?>
+                <?php foreach ($scope->listNonTerminalRootPages() as $page) { ?>
+                    <li class="uk-nav-header"><?=$page->getLabel()?></li>
+                    <?php foreach ($page->listSubPages() as $subPage) { ?>
+                        <li>
+                            <a <?=$subPage->isCurrent()?'class="active"':''?> href="<?=$subPage->getUrl()?>">
+                                <?=$subPage->getMenuLabel()?>
+                            </a>
+                        </li>
+                    <?php } ?>
+                <?php } ?>
+            <?php } elseif ($scope->hasTerminalRootPages()) { ?>
+                <li class="uk-nav-header">Menu</li>
+                <?php foreach ($scope->listTerminalRootPages() as $page) { ?>
                     <li>
-                        <a <?=$subPage->isCurrent()?'class="active"':''?> href="<?=$subPage->getUrl()?>">
-                            <?=$subPage->getMenuLabel()?>
+                        <a <?=$page->isCurrent()?'class="active"':''?> href="<?=$page->getUrl()?>">
+                            <?=$page->getMenuLabel()?>
                         </a>
                     </li>
                 <?php } ?>
             <?php } ?>
-        <?php } elseif ($scope->hasTerminalRootPages()) { ?>
-            <li class="uk-nav-header">Menu</li>
-            <?php foreach ($scope->listTerminalRootPages() as $page) { ?>
+        <?php } ?>
+
+        <?php if ($currentRootPage->hasTerminalSubPages()) { ?>
+            <li class="uk-nav-header"><?=$currentRootPage->getLabel()?></li>
+            <?php foreach ($currentRootPage->listTerminalSubPages() as $page) { ?>
                 <li>
                     <a <?=$page->isCurrent()?'class="active"':''?> href="<?=$page->getUrl()?>">
                         <?=$page->getMenuLabel()?>
@@ -39,30 +63,56 @@ $currentRootPage = $scope->getCurrentRootPage();
                 </li>
             <?php } ?>
         <?php } ?>
-    <?php } ?>
 
-    <?php if ($currentRootPage->hasTerminalSubPages()) { ?>
-        <li class="uk-nav-header"><?=$currentRootPage->getLabel()?></li>
-        <?php foreach ($currentRootPage->listTerminalSubPages() as $page) { ?>
-            <li>
-                <a <?=$page->isCurrent()?'class="active"':''?> href="<?=$page->getUrl()?>">
-                    <?=$page->getMenuLabel()?>
-                </a>
-            </li>
-        <?php } ?>
-    <?php } ?>
-
-    <?php if ($currentRootPage->hasNonTerminalSubPages()) { ?>
-        <?php foreach ($currentRootPage->listNonterminalSubpages() as $page) { ?>
-            <li class="uk-nav-header"><?=$page->getLabel()?></li>
-            <?php foreach ($page->listSubpages() as $subpage) { ?>
-                <li>
-                    <a <?=$subpage->isCurrent()?'class="active"':''?> href="<?=$subpage->getUrl()?>">
-                        <?=$subpage->getMenuLabel()?>
-                    </a>
-                </li>
+        <?php if ($currentRootPage->hasNonTerminalSubPages()) { ?>
+            <?php foreach ($currentRootPage->listNonterminalSubpages() as $page) { ?>
+                <li class="uk-nav-header"><?=$page->getLabel()?></li>
+                <?php foreach ($page->listSubpages() as $subpage) { ?>
+                    <li>
+                        <a <?=$subpage->isCurrent()?'class="active"':''?> href="<?=$subpage->getUrl()?>">
+                            <?=$subpage->getMenuLabel()?>
+                        </a>
+                    </li>
+                <?php } ?>
             <?php } ?>
         <?php } ?>
-    <?php } ?>
 
-</ul>
+        <li>
+            <section class="sidebar-group depth-0"><p class="sidebar-heading"><span>Get started</span> <!----></p>
+                <ul class="sidebar-links sidebar-group-items">
+                    <li><a href="/theme/" aria-current="page" class="sidebar-link">Introduction</a></li>
+                    <li><a href="/theme/setup/" class="sidebar-link">Setup</a></li>
+                    <li><a href="/theme/configuration/" class="sidebar-link">Configuration</a></li>
+                    <li><a href="/theme/scripts/" class="sidebar-link">Scripts &amp; Tasks</a></li>
+                </ul>
+            </section>
+        </li>
+        <li>
+            <section class="sidebar-group depth-0"><p class="sidebar-heading open"><span>Theme</span> <!----></p>
+                <ul class="sidebar-links sidebar-group-items">
+                    <li><a href="/theme/folders/" class="sidebar-link">Folder Structure</a></li>
+                    <li><a href="/theme/functions/" aria-current="page" class="active sidebar-link">Functions</a>
+                        <ul class="sidebar-sub-headers">
+                            <li class="sidebar-sub-header"><a href="/theme/functions/#details" class="sidebar-link">Details</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="/theme/cleanup/" class="sidebar-link">Cleanup</a></li>
+                    <li><a href="/theme/bootstrap/" class="sidebar-link">Bootstrap</a></li>
+                </ul>
+            </section>
+        </li>
+        <li>
+            <section class="sidebar-group depth-0"><p class="sidebar-heading"><span>Assets</span> <!----></p>
+                <ul class="sidebar-links sidebar-group-items">
+                    <li><a href="/theme/css/" class="sidebar-link">CSS</a></li>
+                    <li><a href="/theme/javascript/" class="sidebar-link">JavaScript</a></li>
+                    <li><a href="/theme/images/" class="sidebar-link">Images</a></li>
+                    <li><a href="/theme/fonts/" class="sidebar-link">Fonts</a></li>
+                    <li><a href="/theme/icons/" class="sidebar-link">Icons</a></li>
+                    <li><a href="/theme/localization/" class="sidebar-link">Localization</a></li>
+                </ul>
+            </section>
+        </li>
+    </ul>
+</aside>
